2997952bc97871c140f3724a63a6c7fed6310f57
Alvaro Neira Ayuso <aneira@soleta.eu>
2019-12-16 10:18:12 +0100

    Add global variable to set up OpenGnsys path
    This variable allow us to modify the opengnsys path without modify several parts
    of the code. This change reduces the probability to add any bug forgetting to
    change any line.

--- src/linux/ogOperations.py
+++ src/linux/ogOperations.py
@@ -1,14 +1,16 @@
 import os
 import subprocess
 
+OG_PATH = '/opt/opengnsys/'
+
 def poweroff():
 	if os.path.exists('/scripts/oginit'):
-		subprocess.call('source /opt/opengnsys/etc/preinit/loadenviron.sh; /opt/opengnsys/scripts/poweroff', shell=True)
+		subprocess.call('source ' + OG_SCRIPT_PATH + 'etc/preinit/loadenviron.sh; ' + OG_SCRIPT_PATH + 'scripts/poweroff', shell=True)
 	else:
 		subprocess.call(['/sbin/poweroff'])
 
 def reboot():
 	if os.path.exists('/scripts/oginit'):
-		subprocess.call('source /opt/opengnsys/etc/preinit/loadenviron.sh; /opt/opengnsys/scripts/reboot', shell=True)
+		subprocess.call('source ' + OG_SCRIPT_PATH + 'etc/preinit/loadenviron.sh; ' + OG_SCRIPT_PATH + 'scripts/reboot', shell=True)
 	else:
 		subprocess.call(['/sbin/reboot'])
