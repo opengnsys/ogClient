445497fe01f122f9ae164d686b23e8ef9c481e33
Alvaro Neira Ayuso <aneira@soleta.eu>
2019-12-27 14:39:20 +0100

    Avoid a broken pipe error on opengnsys

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from src.ogClient import *
 from src.ogConfig import *
+from signal import signal, SIGPIPE, SIG_DFL
 
 def main():
+	signal(SIGPIPE, SIG_DFL)
 	ogconfig = ogConfig()
 	if (not ogconfig.parserFile('cfg/ogagent.cfg')):
 		print ('Error: Parsing configuration file')
