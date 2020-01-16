05b1088827ff5b1d5fb5a96bee14f410d78fbc0d
Alvaro Neira Ayuso <aneira@soleta.eu>
2020-01-16 19:32:16 +0100

    Include License header

--- tests/units/__init__.py
+++ tests/units/__init__.py
@@ -1,1 +1,7 @@
-
+#
+# Copyright (C) 2020 Soleta Networks <info@soleta.eu>
+#
+# This program is free software: you can redistribute it and/or modify it under
+# the terms of the GNU Affero General Public License as published by the
+# Free Software Foundation, version 3.
+#
