434bb272b53858e9a9df80986f7617adc80a2f43
Roberto Hueso Gómez <rhueso@soleta.eu>
2020-01-20 12:27:04 +0100

    Add /hardware tests
    This tests covers a correct request.

---
+++ tests/units/test_0004_hardware.py
@@ -0,0 +1,32 @@
+#
+# Copyright (C) 2020 Soleta Networks <info@soleta.eu>
+#
+# This program is free software: you can redistribute it and/or modify it under
+# the terms of the GNU Affero General Public License as published by the
+# Free Software Foundation, version 3.
+#
+
+from server import Server
+from client import Client
+import unittest
+import json
+
+class TestHardwareMethods(unittest.TestCase):
+
+    def test_get(self):
+        req = 'GET /hardware HTTP/1.0\r\nContent-Length:0\r\n' + \
+              'Content-Type:application/json\r\n\r\n '
+        c = Client()
+        s = Server()
+        s.connect()
+        s.send(req)
+        client_response = s.recv()
+        s.stop()
+        c.stop()
+        self.assertRegex(client_response, '^HTTP/1.0 200 OK\r\n*')
+        client_response = client_response.split('\r\n\r\n')
+        response_json = json.loads(client_response[1])
+        self.assertIn('hardware', response_json)
+
+if __name__ == '__main__':
+    unittest.main()
