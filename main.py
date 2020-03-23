f11e345dbf4892ae57fe0ffded5f78febb26dc21
Javier Sanchez Parra <jsanchez@soleta.eu>
2020-03-23 15:58:57 +0100

    Rename config file
    This commit changes the name of the config file from ogagent.cfg to
    ogclient.cfg.

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ from signal import signal, SIGPIPE, SIG_DFL
 def main():
 	signal(SIGPIPE, SIG_DFL)
 	ogconfig = ogConfig()
-	config_path = f'{ogConfig.OG_PATH}ogClient/cfg/ogagent.cfg'
+	config_path = f'{ogConfig.OG_PATH}ogClient/cfg/ogclient.cfg'
 	if (not ogconfig.parser_file(config_path)):
 		print ('Error: Parsing configuration file')
 		return 0
