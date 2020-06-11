118572c3b6fc5a1f2ad0ab1cd46fe5e00bf283a7
Roberto Hueso Gómez <rhueso@soleta.eu>
2020-06-11 14:05:51 +0200

    Add setup.py installation script
    This script can be useful to install ogclient on distributions that do not have
    an already built ogclient package.
    
    It is necessary to install ogclient in the right path, so the command you have
    to use looks similar to:
    python3 setup.py install --prefix=/opt/opengnsys/ogclient
                             --install-lib=/opt/opengnsys/ogclient
                             --install-script=/opt/opengnsys/ogclient

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(name='ogclient',
+      version='1.0',
+      description='Client for the OpenGnsys ecosystem',
+      author='Soleta OpenGnsys Support Team',
+      author_email='soporte-og@soleta.eu',
+      url='https://github.com/opengnsys/ogClient',
+      packages=['src', 'src.linux', 'src.virtual'],
+      scripts=['main.py'],
+      data_files=[('cfg', ['cfg/ogclient.json']),
+                  ('', ['LICENSE'])]
+)
