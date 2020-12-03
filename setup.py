opengnsys/ogClient

commit 1377aceec23fdb0efc4e1406ee308e1377d36065

Rename 'linux' mode to 'live' mode
'linux' represents ogLive mode that is the reason for the rename.

src/ogRest.py
@@ -238,7 +238,7 @@ class ogRest():
 		self.mode = self.CONFIG['opengnsys']['mode']
 		self.samba_config = self.CONFIG['samba']
 
-		if self.mode == 'linux':
+		if self.mode == 'live':
 			self.operations = OgLinuxOperations(self.CONFIG)
 		elif self.mode == 'virtual':
 			from src.virtual.ogOperations import \

main.py
@@ -25,7 +25,7 @@ def main():
 
 	MODE = CONFIG['opengnsys']['mode']
 	URL = CONFIG['opengnsys']['url']
-	if MODE == 'linux':
+	if MODE == 'live':
 		proc = subprocess.Popen(["browser", "-qws", URL])
 
 	client = ogClient(config=CONFIG)

src/ogClient.py
@@ -30,7 +30,7 @@ class ogClient:
 		self.CONFIG = config
 
 		self.mode = self.CONFIG['opengnsys']['mode']
-		if self.mode not in {'virtual', 'linux'}:
+		if self.mode not in {'virtual', 'live'}:
 			raise ValueError('Mode not supported.')
 
 		if self.CONFIG['samba']['activate']:

commit f0aa3df485172e8419d37186b08cd480f0a2a7d9

Rename 'linux' folder and operations to 'live'
ogLive related operations are named inside a 'Linux' folder, also its
python class is named OgLinuxOperations. Rename every 'linux' occurrence
with live to further clarify this folder and operations.

 - OgLinuxOperations -> OgLiveOperations
 - src/linux/ -> src/live/

Fixes: 1377acee ('Rename 'linux' mode to 'live' mode')

setup.py
@@ -6,7 +6,7 @@ setup(name='ogclient',
       author='Soleta OpenGnsys Support Team',
       author_email='soporte-og@soleta.eu',
       url='https://github.com/opengnsys/ogClient',
-      packages=['src', 'src.linux', 'src.virtual'],
+      packages=['src', 'src.live', 'src.virtual'],
       scripts=['ogclient'],
       data_files=[('cfg', ['cfg/ogclient.json']),
                   ('', ['COPYING'])]

src/live/ogOperations.py
@@ -13,7 +13,7 @@ from src.ogClient import ogClient
 
 OG_SHELL = '/bin/bash'
 
-class OgLinuxOperations:
+class OgLiveOperations:
     def __init__(self, config):
         self._url = config['opengnsys']['url']
         self._url_log = config['opengnsys']['url_log']

src/ogRest.py
@@ -256,8 +256,8 @@ class ogRest():
 		self.samba_config = self.CONFIG['samba']
 
 		if self.mode == 'live':
-			from src.linux.ogOperations import OgLinuxOperations
-			self.operations = OgLinuxOperations(self.CONFIG)
+			from src.live.ogOperations import OgLiveOperations
+			self.operations = OgLiveOperations(self.CONFIG)
 		elif self.mode == 'virtual':
 			from src.virtual.ogOperations import \
 				OgVirtualOperations