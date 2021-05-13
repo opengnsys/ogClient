cb9edc8d95300085aeb361bb0a3f81f268531542
OpenGnSys Support Team <soporte-og@soleta.eu>
2021-05-14 00:19:28 +0200

    ogClient is AGPLv3+
    Update license header in files.

--- src/live/__init__.py
+++ src/live/__init__.py
@@ -1,7 +1,1 @@
-#
-# Copyright (C) 2020 Soleta Networks <info@soleta.eu>
-#
-# This program is free software: you can redistribute it and/or modify it under
-# the terms of the GNU Affero General Public License as published by the
-# Free Software Foundation, version 3.
-#
+
