opengnsys/ogClient

commit 700aa89ea99c7d48350fb99c947c60fc02c0323e

#1065 Use getattr when retrieving event socket
If ogClient does not run on windows or linux mode, it does not create a
datagram event socket.

If ogClient runs in virtual or live mode this will cause an error when
calling get_event_socket because the class instance has no member
"event_sock".

Use getattr when retrieving the event socket from ogClient class, if
there is no event socket, then return None.

Fixes 2465ef25b741 (Add event datagram socket)

src/ogClient.py
@@ -48,7 +48,7 @@ class ogClient:
 		return self.sock
 
 	def get_event_socket(self):
-		return self.event_sock
+		return getattr(self, 'event_sock', None)
 
 	def get_state(self):
 		return self.state

commit fa0e48a778ceedaad6a1461dda1532a7db9b6579

#1065 Init event socket according to mode
Commit 700aa89ea99 introduced the use of getattr when getting event
socket to avoid errors in case it was not initialized.

Prefer to initialize accordingly inside the ogClient class constructor.

src/ogClient.py
@@ -35,6 +35,8 @@ class ogClient:
 			self.event_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 			self.event_sock.setblocking(0)
 			self.event_sock.bind(('127.0.0.1', 55885))
+		else:
+			self.event_sock = None
 
 		if self.CONFIG['samba']['activate']:
 			assert('user' in self.CONFIG['samba'])
@@ -48,7 +50,7 @@ class ogClient:
 		return self.sock
 
 	def get_event_socket(self):
-		return getattr(self, 'event_sock', None)
+		return self.event_sock
 
 	def get_state(self):
 		return self.state